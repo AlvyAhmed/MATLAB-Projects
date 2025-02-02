e8ba00b370e235930e5b88fbe6ed35720453a796
Alvy Ahmed <tamzeedahmed@iut-dhaka.edu>
2025-02-02 15:01:49 +0600

    Add files via upload

---
+++ Bending_moment_diagram.m
@@ -0,0 +1,14 @@
+w=20;
+l = 4;
+x =[0:0.1:4];
+v =(-(w*x)+((w*l)/2));
+m= -((w*x.^2)/2)+((w*l*x)/2);
+plot(x,v)
+xlabel('x(m)')
+ylabel('Shear force (kN)')
+title('Shear force diagam')
+figure
+plot(x,m)
+xlabel('x(m)')
+ylabel('Bending moment (kN-m)')
+title('Bending Moment Diagram')
